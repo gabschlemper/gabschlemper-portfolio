804e9f00fbe33bde466fc5450d40ae6528c780a8
Gabriela Schlemper <gabschlemper@gmail.com>
2023-10-27 18:47:24 -0300

    Fix header

--- src/pages/work/WorkDetail.js
+++ src/pages/work/WorkDetail.js
@@ -14,7 +14,7 @@ const WorkDetail = ({ id }) => {
     <>
       <Header />
       <div className="pt-24 md:pt-32">
-        <div className="flex flex-col items-center justify-center py-20">
+        <div className="flex flex-col items-center justify-center py-20 h-96">
           <h1 className="text-2xl lg:text-3xl uppercase pb-5 ">{item.title}</h1>
           <h3 className="pb-6 text-grey-100 max-w-xl leading-8 text-center lg:leading-10 lg:text-lg">
             {item.overview}
